cdf4194d84d879b4987e04e5ed66fb7154377291
Agata <72201069+coffeinna@users.noreply.github.com>
2026-01-29 16:58:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if ingredients_list:
     #st.text(ingredients_list)
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-      ingredients_string += fruit_chosen + ', '
+      ingredients_string += fruit_chosen + ' '
       
       search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       
