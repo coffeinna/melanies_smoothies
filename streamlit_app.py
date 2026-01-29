fa73bb8944b874f86794882bf1946a946e30f8e8
Agata <72201069+coffeinna@users.noreply.github.com>
2026-01-29 14:54:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
 
 # Write directly to the app
 st.title(f":cup_with_straw: Smoothie! :cup_with_straw: ") #{st.__version__}
@@ -50,3 +48,11 @@ if ingredients_list:
 
 #option = st.selectbox('Fruit:',('Banana','Strawberry','Peach','Mango'))
 #st.write('Your choose:',option)
+
+
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
